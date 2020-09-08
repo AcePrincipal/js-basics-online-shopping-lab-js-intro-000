2d0da99638390d0a342b5579f86e8d83d68caea8
Alexander Petrov <acepetrov96@gmail.com>
2020-09-08 17:44:20 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ function setCart(c) {
 
 function addToCart(item) {
  // write your code here
-  var obj = { itemName: item, itemPrice: Math.floor(Math.random*100+1)}
+  var obj = { itemName: item, itemPrice: Math.floor(Math.random()*100+1)}
   cart.push(obj);
 
   return `${item} has been added to your cart.`;
@@ -41,12 +41,36 @@ function viewCart() {
 
 function total() {
   // write your code here
+  var total1 = 0;
+  for(var i = 0; i < cart.length; i++) {
+    total1 += cart[i].itemPrice;
+  }
+
+  return total1;
 }
 
 function removeFromCart(item) {
   // write your code here
+  var bool = false;
+
+  for(var i = 0; i < cart.length; i++) {
+    if(cart[i].itemName === item) {
+      cart.splice(1, i);
+      bool = true;
+    }
+  }
+  if(bool === false) {
+    return 'That item is not in your cart.';
+  }
+
+  return cart;
 }
 
 function placeOrder(cardNumber) {
   // write your code here
+  if(cardNumber === undefined) {
+    return 'Sorry, we don\'t have a credit card on file for you.';
+  }
+  cart.splice(0, cart.length);
+  return `Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`
 }
